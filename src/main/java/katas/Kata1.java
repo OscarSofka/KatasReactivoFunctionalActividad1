eed57ae04b485e71409113f1b304fdfca92b15af
OscarSofka <107581543+OscarSofka@users.noreply.github.com>
2022-07-08 14:37:15 -0500

    Katas

---
+++ src/main/java/katas/Kata1.java
@@ -0,0 +1,29 @@
+package katas;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import model.Movie;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/*
+    Goal: use map() to project an array of videos into an array of {id, title}-pairs
+    DataSource: DataUtil.getMovies()
+    Output: List of ImmutableMap.of("id", "5", "title", "Bad Boys")
+*/
+public class Kata1 {
+
+    public static List<ImmutableMap<Integer, String>> execute() {
+        List<Movie> movies = DataUtil.getMovies();
+
+        return movies.stream().map(Kata1::apply).collect(Collectors.toList());
+    }
+
+    private static ImmutableMap<Integer, String> apply(Movie movie) {
+        return ImmutableMap.of(movie.getId(),movie.getTitle());
+    }
+
+}
